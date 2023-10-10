459ab315f0375c9991816459b572c4253a7215ce
Poniac <113772161+Poniac@users.noreply.github.com>
2023-10-10 15:21:56 +0300

    Update index.js

--- task_1/index.js
+++ task_1/index.js
@@ -1,6 +1,22 @@
 import {encoded, translations} from './data.js'
-
-console.log("Let's rock")
+function Let's rock(text) {
+let letter, count, i, result, letters;
+result = "";
+for (i = 0; i < text.length; i++) {
+        count = text[i];
+        var parsedNumber = Number(count);
+        if (!Number.isNaN(parsedNumber)) {               
+            letter = text[i + 1];
+            letters = letter.repeat(parsedNumber);
+            result += letters;
+            console.log(result);  // this line only for testing
+        } else {
+            result += text[i];
+        }
+    }
+    return result;
+}
+console.log("Let's rock")"v2rt3b2x"));
 console.log(encoded, translations)
 
 
